56be6e45c556fc9e580644676b7dddb160d98ffd
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-20 10:52:39 -0500

    initial commit

---
+++ scripts/initController.js
@@ -0,0 +1,11 @@
+(function() {
+    function initContoller() {
+        var vm = this;
+
+    }
+
+
+    angular
+        .module("app")
+        .controller("initController", initController)
+})();
---
+++ scripts/module.js
@@ -0,0 +1,17 @@
+(function() {
+    angular
+        .module("app", ["ngRoute"])
+        .config(function($routeProvider) {
+            $routeProvider
+                .when("/group", {
+                    //template: component
+
+                })
+                .when("/weather", {
+
+                })
+                .when("/prime", {
+
+                })
+        })
+})();

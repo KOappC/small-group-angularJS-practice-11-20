e9235a6fade5063a19994c5c202c5f3cbcd81c9d
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-21 16:39:57 -0500

    finished the weather view

--- scripts/weatherController.js
+++ scripts/weatherController.js
@@ -3,12 +3,14 @@
         var vm = this;
         vm.currentTemp = "";
         vm.windSpeed = "";
+        vm.windDirection = "";
         vm.locationSearch = function(city, state) {
             console.log(city, state);
             weatherFactory.makeRequest(city,state).then(function(response) {
                 console.log(response);
                 vm.currentTemp = response.data.current_observation.temp_f;
                 vm.windSpeed = response.data.current_observation.wind_mph;
+                vm.windDirection = response.data.current_observation.wind_dir;
             })
         }
 
